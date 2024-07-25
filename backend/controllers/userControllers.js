6d6471f0fb5e39da87e5217625a3a42b64401dcd
Nacef19 <117386346+Nacef19@users.noreply.github.com>
2024-07-26 00:49:31 +0100

    Add error handling to loginUser function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -34,21 +34,17 @@ const createUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
-    if (isPasswordValid) {
-      createToken(res, existingUser._id);
-      res.status(201).json({
-        _id: existingUser._id,
-        userName: existingUser.userName,
-        email: existingUser.email,
-        isAdmin: existingUser.isAdmin,
-      });
-      return;
-    }
+
+  if (existingUser && (await bcrypt.compare(password, existingUser.password))) {
+    createToken(res, existingUser._id);
+    res.status(200).json({
+      _id: existingUser._id,
+      userName: existingUser.userName,
+      email: existingUser.email,
+      isAdmin: existingUser.isAdmin,
+    });
+  } else {
+    res.status(401).json({ message: "Invalid email or password!" });
   }
 });
 
